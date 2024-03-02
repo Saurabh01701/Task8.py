11c516e31fa3f369401950567a09f81c473f0bd5
Saurabh vishwakarma <161301738+Saurabh01701@users.noreply.github.com>
2024-03-02 13:03:28 +0530

    Add files via upload

---
+++ task8.py
@@ -0,0 +1,10 @@
+marks=int(input("Enter your marks:"))
+if marks>=90:
+  print("A")
+elif marks<90 and marks>=75:
+  print("B")
+elif marks<75 and marks>=60:
+  print("C")
+else:
+  print("Fail")
+  
